f19448ec9fd74a9bd1cb92087211a5822366c3a9
MDJAmin <mohammadamindf@gmail.com>
2024-07-16 17:22:09 +0430

    add constants & utils

---
+++ src/constants/index.js
@@ -0,0 +1,56 @@
+import {
+  highlightFirstVideo,
+  highlightFourthVideo,
+  highlightSecondVideo,
+  highlightThirdVideo,
+} from "../utils";
+
+export const navLists = ["Products", "Solutions", "Community", "Support"];
+
+export const hightlightsSlides = [
+  {
+    id: 1,
+    textLists: [
+      "Enter A17 Pro.",
+      "Game‑changing chip.",
+      "Groundbreaking performance.",
+    ],
+    video: highlightFirstVideo,
+    videoDuration: 4,
+  },
+  {
+    id: 2,
+    textLists: ["Titanium.", "So strong. So light. So Pro."],
+    video: highlightSecondVideo,
+    videoDuration: 5,
+  },
+  {
+    id: 3,
+    textLists: [
+      "MSI's latest PC has the highest",
+      "performance in gaming ever!",
+      "Unleash the power.",
+    ],
+    video: highlightThirdVideo,
+    videoDuration: 2,
+  },
+  {
+    id: 4,
+    textLists: ["All-new Action button.", "What will yours do?."],
+    video: highlightFourthVideo,
+    videoDuration: 3.63,
+  },
+];
+
+export const sizes = [
+  { label: '6.1"', value: "small" },
+  { label: '7"', value: "large" },
+];
+
+export const footerLinks = [
+  "Privacy Policy",
+  "Terms of Use",
+  "Sales Policy",
+  "Legal",
+  "Site Map",
+];
