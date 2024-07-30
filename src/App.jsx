a9180f968b2c1ee7c43c316674391d51105ead24
MDJAmin <mohammadamindf@gmail.com>
2024-07-30 20:17:50 +0430

    remove slider section

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
     <main className="bg-black">
       <Navbar />
       <Hero />
-      <Popular />
+      {/* <Popular /> */}
       <Model />
       <Tech />
       <Features />
