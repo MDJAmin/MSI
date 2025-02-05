ff6e5e19020c7194e13875e94ec181ba1329d310
MDJAmin <mohammadamindf@gmail.com>
2025-02-05 08:33:05 +0330

    Fix: GLTF file path (PC Model)

--- src/components/Model3D/Model3D.jsx
+++ src/components/Model3D/Model3D.jsx
@@ -3,7 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 
 function Model() {
-  const { scene } = useGLTF('/models/pc.glb'); // Adjust the path if needed
+  const { scene } = useGLTF('/MSI/models/pc.glb'); // Adjust the path if needed
 
   console.log('Model scene:', scene); // Check if materials are present in the console
 
@@ -11,7 +11,7 @@ function Model() {
 }
 
 // Preload the GLTF file
-useGLTF.preload('/models/scene.gltf');
+useGLTF.preload('/MSI/models/scene.gltf');
 
 export default function Model3D() {
   return (
@@ -20,7 +20,7 @@ export default function Model3D() {
         <ambientLight intensity={5}/>
         <pointLight position={[10, 10, 10]} />
         <Model />
-        {/* <OrbitControls enableZoom={false}  maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2}/> */}
+        <OrbitControls enableZoom={false}  maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2}/>
         <OrbitControls enableZoom={false} />
       </Suspense>
     </Canvas>
