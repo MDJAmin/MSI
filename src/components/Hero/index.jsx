f7e4f91ed3bf142398cec6264c1fac57d6cc951d
MDJAmin <mohammadamindf@gmail.com>
2025-02-06 11:17:19 +0330

    Refactor: small hero Video
    add a small hero Video for small devices, handle the resize with useState and useEffect.

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,50 +1,40 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import React, { useState } from "react";
-import { heroVideo } from "../../utils";
+import React, { useState, useEffect } from "react";
+import { heroVideo, smallHeroVideo } from "../../utils";
 import "./style.module.css";
+
 export default function Hero() {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   useGSAP(() => {
     gsap.fromTo(
-      ".hero-title",
-      {
-        opacity: 0,
-        y: -50,
-      },
-      {
-        opacity: 0.9,
-        y: 0,
-        duration: 4,
-      },
-      gsap.fromTo(
-        ".btn",
-        {
-          opacity: 0,
-          scrub: true,
-        },
-        { opacity: 0.9, duration: 3, delay: 1, scrub: true }
-      )
+      ".btn",
+      { opacity: 0, y: 20 },
+      { opacity: 1, duration: 3, delay: 1, y: -20 }
     );
   }, []);
+
   return (
     <section className="w-full nav-height bg-black">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p className="text-5xl font-bold tracking-widest hero-title">
-          MSI Compony
-        </p>
-        <div className=" md:w-10/12 w-9/12">
-          <video
-            className="pointer-events-none"
-            autoPlay
-            muted
-            playsInline={true}
-          >
-            <source src={heroVideo} type="video/mp4" />
+        <div className="md:w-10/12 w-9/12">
+          <video className="pointer-events-none" autoPlay muted playsInline key={isSmallScreen ? "small" : "large"}>
+            <source src={isSmallScreen ? smallHeroVideo : heroVideo} type="video/mp4" />
           </video>
         </div>
       </div>
 
-      <div id="" className="flex flex-col items-center relative">
+      <div className="flex flex-col items-center relative">
         <a href="#Popular" className="btn">
           See More!
         </a>
