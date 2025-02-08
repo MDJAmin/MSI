2da866426f886628ce60c94acf380a052538feac
MDJAmin <mohammadamindf@gmail.com>
2025-02-08 18:18:13 +0330

    Refactor: Features section (improve)

--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -1,7 +1,11 @@
 import { useGSAP } from "@gsap/react";
 import React, { useRef } from "react";
 import { animateWithGsap } from "../../utils/animations";
-import { explore1Img, explore2Img, exploreVideo } from "../../utils";
+import {
+  explore1Img,
+  explore2Img,
+  exploreVideo,
+} from "../../utils";
 import gsap from "gsap";
 
 const Features = () => {
@@ -19,50 +23,90 @@ const Features = () => {
       },
     });
 
-    animateWithGsap("#features_title", { y: 0, opacity: 1 });
-    animateWithGsap(".g_grow", { scale: 1, opacity: 1, ease: "power1" }, { scrub: 5.5 });
-    animateWithGsap(".g_text", { y: 0, opacity: 1, ease: "power2.inOut", duration: 1 });
+    animateWithGsap("#features_title", {
+      y: 0,
+      opacity: 1,
+    });
+    animateWithGsap(
+      ".g_grow",
+      { scale: 1, opacity: 1, ease: "power1" },
+      { scrub: 5.5 }
+    );
+    animateWithGsap(".g_text", {
+      scrollTrigger: {
+        trigger: ".g_text",
+        start: "top bottom", // Animation starts when the top of the element hits the bottom of the viewport
+      },
+      y: 0,
+      opacity: 1,
+      ease: "power2.inOut",
+      duration: 0.5, 
+    });
   }, []);
 
   return (
-    <section className="h-full common-padding bg-zinc relative overflow-hidden">
-      <div className="screen-max-wdith">
-        <div className="mb-12 mt-24 w-full">
-          <h1 id="features_title" className="section-heading ps-60">
+    <section className='h-full common-padding bg-zinc relative overflow-hidden'>
+      <div className='screen-max-wdith'>
+        <div className='mb-12 mt-24 w-full'>
+          <h1
+            id='features_title'
+            className='section-heading ps-60'
+          >
             Explore the full story.
           </h1>
         </div>
 
-        <div className="flex flex-col justify-center items-center overflow-hidden">
-          <div className="flex-center flex-col sm:px-10">
-            <div className="relative h-[40vh] mb-10 w-[91%] flex items-center">
-              <img src={exploreVideo} alt="titanium" className="feature-video g_grow" />
+        <div className='flex flex-col justify-center items-center overflow-hidden'>
+          <div className='flex-center flex-col sm:px-10'>
+            <div className='relative h-[40vh] mb-10 w-[91%] flex items-center'>
+              <img
+                src={exploreVideo}
+                alt='titanium'
+                className='feature-video g_grow'
+              />
             </div>
 
-            <div className="flex flex-col w-full relative">
-              <div className="feature-video-container">
-                <div className="overflow-hidden flex-1 h-[50vh]">
-                <img src={explore1Img} alt="titanium 2" className="feature-video g_grow" />
+            <div className='flex flex-col w-full relative'>
+              <div className='feature-video-container'>
+                <div className='overflow-hidden flex-1 h-[50vh]'>
+                  <img
+                    src={explore1Img}
+                    alt='titanium 2'
+                    className='feature-video g_grow'
+                  />
                 </div>
-                <div className="overflow-hidden flex-1 h-[50vh]">
-                  <img src={explore2Img} alt="titanium 2" className="feature-video g_grow" />
+                <div className='overflow-hidden flex-1 h-[50vh]'>
+                  <img
+                    src={explore2Img}
+                    alt='titanium 2'
+                    className='feature-video g_grow'
+                  />
                 </div>
               </div>
 
-              <div className="feature-text-container">
-                <div className="flex-1 flex-center">
-                  <p className="feature-text g_text">
+              <div className='feature-text-container'>
+                <div className='flex-1 flex-center'>
+                  <p className='feature-text g_text'>
                     MSI's new motherboards are{" "}
-                    <span className="text-white">the first to feature a 10-layer PCB design</span>
-                    , providing unparalleled signal integrity and stability.
+                    <span className='text-white'>
+                      the first to feature a 10-layer PCB
+                      design
+                    </span>
+                    , providing unparalleled signal
+                    integrity and stability.
                   </p>
                 </div>
 
-                <div className="flex-1 flex-center">
-                  <p className="feature-text g_text">
-                    With a focus on overclocking, MSI's new motherboards boast{" "}
-                    <span className="text-white">the most advanced power delivery system</span>
-                    , ensuring stable power to your CPU and RAM.
+                <div className='flex-1 flex-center'>
+                  <p className='feature-text g_text'>
+                    With a focus on overclocking, MSI's new
+                    motherboards boast{" "}
+                    <span className='text-white'>
+                      the most advanced power delivery
+                      system
+                    </span>
+                    , ensuring stable power to your CPU and
+                    RAM.
                   </p>
                 </div>
               </div>
